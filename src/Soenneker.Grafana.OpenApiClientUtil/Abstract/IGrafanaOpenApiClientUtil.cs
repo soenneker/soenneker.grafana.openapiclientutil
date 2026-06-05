f334c13cd8ed7f3459c209763824c165ecd5e53b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:16 -0500

    xml commenting

--- src/Soenneker.Grafana.OpenApiClientUtil/Abstract/IGrafanaOpenApiClientUtil.cs
+++ src/Soenneker.Grafana.OpenApiClientUtil/Abstract/IGrafanaOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Grafana.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IGrafanaOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<GrafanaOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
